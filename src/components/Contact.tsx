935dc123b83d403c7f854a05dc30b61bfdc5ba5c
Filipe Leoni <filipe_sempre@outlook.com>
2023-08-09 19:21:03 -0300

    mailto added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 import { motion } from "framer-motion";
 import { AiFillInstagram } from "react-icons/ai";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
@@ -33,14 +34,17 @@ export const Contact = ({ title, button }: ContactProps) => {
       </motion.div>
       <motion.div
         className="z-20 w-full max-w-4xl backdrop-blur-sm dark:bg-neutral-900/80 dark:border-gray-700 bg-white/70 drop-shadow-lg border flex-col items-center h-64 rounded-lg justify-center flex"
-        variants={fadeIn("up", 0.1)}
+        variants={fadeIn("up", 0)}
         initial={"hidden"}
         whileInView={"show"}
         viewport={{ once: true }}
       >
-        <div className="items-center justify-center flex cursor-pointer px-16  bg-gray-300 dark:bg-gray-600 font-medium text-gray-800 dark:text-gray-300 h-12 rounded-lg">
+        <Link
+          href="mailto:filipe.dev34@hotmail.com"
+          className="items-center justify-center flex cursor-pointer px-16  bg-gray-300 dark:bg-gray-600 font-medium text-gray-800 dark:text-gray-300 h-12 rounded-lg"
+        >
           {button}
-        </div>
+        </Link>
         <div className="flex mt-6 justify-center items-center gap-4 text-gray-700 dark:text-gray-400">
           <a
             href="https://www.linkedin.com/in/filipe-leoni/"
